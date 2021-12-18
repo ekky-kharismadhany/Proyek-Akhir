592230417ea733d2c524755d92ace9365f93c6e2
ekharisma <ekharisma@outlook.com>
2021-12-18 19:05:46 +0700

    add upload file csv funtionality

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
+from logging import error
 from flask import Flask, request, jsonify
-import json
-
-from backend.services.ClassificationService import handleClassification
 from services.LocationService import LocationService
+import os
 
 app = Flask(__name__)
 
@@ -10,14 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     welcome = {"message": "Hello world"}
-    return json.dumps(welcome)
+    return jsonify(welcome)
 
 
-@app.route("/classification", methods=['POST'])
+@app.route("/classification/csv", methods=['POST'])
 def loads_csv():
-    result = handleClassification(request.get_json())
-    payload = {"message": result}
-    return json.dumps(payload)
+    if request.files:
+        uploaded_file = request.files['filename']
+        file_path = os.path.join(app.config['FILE_UPLOAD'], uploaded_file.filename)
+        uploaded_file.save(file_path)
 
 @app.route("/location", methods=['GET', 'POST'])
 def location():
@@ -30,4 +30,8 @@ def location():
 
 
 if __name__ == "__main__":
+    app.config['FILE_UPLOAD'] =  os.getcwd() + "/uploads/"
+    app.debug = True
+    if not os.path.isdir(app.config['FILE_UPLOAD']):
+        raise RuntimeError("Upload path does not exist")
     app.run()
