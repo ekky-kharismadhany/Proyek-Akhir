2a691df2c6b5eecf069d61e5f55ff32d0c213fce
ekharisma <ekharisma@outlook.com>
2022-01-04 16:16:47 +0700

    cors added to backend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,22 @@
-from logging import error
 from flask import Flask, request, jsonify, make_response
-from scipy.sparse import csc
+from flask_cors import CORS, cross_origin
 from services.ClassificationService import ClassificationService
 from services.LocationService import LocationService
 from services.FileService import FileService
 import os
 
 app = Flask(__name__)
-
+cors = CORS(app)
 
 @app.route("/")
+@cross_origin()
 def index():
     welcome = {"message": "Hello world"}
     return jsonify(welcome)
 
 
 @app.route("/classification/csv", methods=['GET', 'POST'])
+@cross_origin()
 def upload_csv():
     if request.method == 'POST':
         if request.files:
@@ -25,7 +26,7 @@ def upload_csv():
             return jsonify({
                 'status': status,
                 'message': message
-                })
+            })
         return jsonify({
             'status': False,
             'message': 'No file uploaded'
@@ -38,17 +39,20 @@ def upload_csv():
         response.headers["Content-type"] = "text/csv"
         return response
 
+
 @app.route("/classification")
+@cross_origin()
 def classify():
     classification_service = ClassificationService()
     return jsonify({
-        'status' : True,
-        'result' : classification_service.get_result_by_attack_type(),
+        'status': True,
+        'result': classification_service.get_result_by_attack_type(),
         'metric': classification_service.get_metric()
     })
 
 
 @app.route("/location", methods=['POST'])
+@cross_origin()
 def location():
     ip: str = request.json['ip']
     location = LocationService()
@@ -56,14 +60,15 @@ def location():
     location.set_location_by_ip()
     result: dict[str:str] = location.get_map_location()
     return jsonify({
-        'status' : True,
+        'status': True,
         'message': result
     })
 
 
 if __name__ == "__main__":
     app.config['FILE_UPLOAD'] = os.getcwd() + "/uploads/"
-    app.debug = True
+    # app.debug = False
+    app.secret_key = '123'
     if not os.path.isdir(app.config['FILE_UPLOAD']):
         raise RuntimeError("Upload path does not exist")
     app.run()
